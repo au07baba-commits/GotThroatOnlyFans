a2daebb9844b4edcc0ae5477763e9e0eec4919ad
au07baba-commits <au07baba@gmail.com>
2026-04-27 08:51:10 +0100

    Add files via upload

---
+++ me2/pro/script.js
@@ -0,0 +1,25 @@
+function payNow() {
+    alert("Payment system coming soon. Connect Paystack or Flutterwave.");
+}
+
+function unlockContent() {
+    alert("This content is locked. Complete payment to unlock.");
+}
+
+function openWhatsApp() {
+    window.open("https://wa.me/2349071513015", "_blank");
+}
+
+function openFacebook() {
+    window.open("https://facebook.com", "_blank");
+}
+
+function openTelegram() {
+    window.open("https://t.me/Mainaccount28", "_blank");
+}
+
+function scrollToGallery() {
+    document.getElementById("gallery").scrollIntoView({
+        behavior: "smooth"
+    });
+}
